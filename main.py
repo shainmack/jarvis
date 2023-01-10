00fb78f90ff2264e5404916d0ece62f730cd662f
Shain Toth <shaintoth@icloud.com>
2023-01-10 10:22:23 -0500

    added openai access keys

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
-import openai
+import openai as ai
 import speech_recognition
 import pyaudio
 import boto3
 import pydub
+
+ai.organization = 'org-YB5CGIuUYASqH4kd334vMaum'
+ai.api_key = os.environ.get('OPENAI_API_KEY')
 
 if __name__  == "__main__" :
     print("It works!")
