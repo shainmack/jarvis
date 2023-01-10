4fd305989fe321c783beefe9694ee34bc7f4f463
Shain Toth <shaintoth@icloud.com>
2023-01-10 10:48:24 -0500

    Received successful response from openai

--- main.py
+++ main.py
@@ -8,5 +8,20 @@ import pydub
 ai.organization = 'org-YB5CGIuUYASqH4kd334vMaum'
 ai.api_key = os.environ.get('OPENAI_API_KEY')
 
+def query_ai(prompt):
+    print('Prompt received: ', prompt)
+    completions = ai.Completion.create(
+        engine = 'text-davinci-003',
+        prompt = prompt,
+        max_tokens = 128,
+        n = 1,
+        stop = None,
+        temperature = 0.5
+    )
+    message = completions.choices[0].text
+    print('Message: ', message)
+    return message
+
+
 if __name__  == "__main__" :
-    print("It works!")
+    query_ai('how are you?')
